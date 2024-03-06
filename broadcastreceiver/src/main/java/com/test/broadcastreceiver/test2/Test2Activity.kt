e0802ed70be2620baafb25bfd837474f27fb424c
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2024-03-06 11:16:30 +0900

    feat: BroadcastReceiver 스크린 on/off시 반응 구현

--- broadcastreceiver/src/main/java/com/test/broadcastreceiver/test2/Test2Activity.kt
+++ broadcastreceiver/src/main/java/com/test/broadcastreceiver/test2/Test2Activity.kt
@@ -1,12 +1,38 @@
 package com.test.broadcastreceiver.test2
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.test.broadcastreceiver.R
 
 class Test2Activity : AppCompatActivity() {
+
+    lateinit var receiver: BroadcastReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test2)
+
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                when(intent?.action) {
+                    Intent.ACTION_SCREEN_ON -> Log.d("kkang", "screen on..")
+                    Intent.ACTION_SCREEN_OFF -> Log.d("kkang", "screen of....")
+                }
+            }
+        }
+        val filter = IntentFilter().apply {
+            addAction(Intent.ACTION_SCREEN_ON)
+            addAction(Intent.ACTION_SCREEN_OFF)
+        }
+        registerReceiver(receiver, filter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
