21f2cb5ccbec1df551c951df4188aeb29a92c373
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2024-03-07 17:38:53 +0900

    feat: contentProvider query 구현

---
+++ contentprovider/src/main/java/com/test/contentprovider/test1/MyContentProvider.kt
@@ -0,0 +1,37 @@
+package com.test.contentprovider.test1
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.database.MatrixCursor
+import android.net.Uri
+
+class MyContentProvider : ContentProvider() {
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        return 0
+    }
+
+    override fun getType(uri: Uri): String? = null
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? = null
+
+    override fun onCreate(): Boolean = true
+
+    override fun query(
+        uri: Uri, projection: Array<String>?, selection: String?,
+        selectionArgs: Array<String>?, sortOrder: String?
+    ): Cursor? {
+        val cursor = MatrixCursor(arrayOf("data"))
+        cursor.addRow(arrayOf("hello"))
+        cursor.addRow(arrayOf("world"))
+        return cursor
+    }
+
+    override fun update(
+        uri: Uri, values: ContentValues?, selection: String?,
+        selectionArgs: Array<String>?
+    ): Int {
+        return 0
+    }
+}
---
+++ contentprovider/src/main/java/com/test/contentprovider/test1/Test1Activity.kt
@@ -0,0 +1,25 @@
+package com.test.contentprovider.test1
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.test.contentprovider.R
+import com.test.contentprovider.databinding.ActivityTest1Binding
+
+class Test1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityTest1Binding.inflate(layoutInflater)
+        setContentView(R.layout.activity_test1)
+
+        binding.button.setOnClickListener {
+            val cursor = contentResolver.query(
+                Uri.parse("contet://com.my.provider"),
+            null,null,null,null,null)
+            while(cursor?.moveToNext() ?: false){
+                Log.d("kkang",cursor?.getString(0) ?: "")
+            }
+        }
+    }
+}
